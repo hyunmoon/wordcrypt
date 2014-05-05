6c732862d1d37489771c1104451da87fa930e023
KevinPeng <kevinpeng0@gmail.com>
2014-05-05 18:05:12 -0400

    Added file option input

--- main.c
+++ main.c
@@ -18,17 +18,22 @@ int main(int argc, char *argv[]){
 	int opt;
 	char de_or_en = 'e';
 
+	char * filename;
+	char * endptr;
+
 	int dflag = 0;
 	int eflag = 0;
 
+	int val;
 	if(stdin == NULL){
 		fprintf(stderr, "Error: File not found");
 		exit(1);
 	}
 	while((opt = getopt(argc,argv,"dehvf:p:")) != -1){
 		switch(opt){
-			case 'd'://decrypt
+			case 'd'://decrypt, mutually exclusive with encrypt
 				if(eflag){
+					//exit out with error as it is mutually exclusive with encrypt option
 					usage(EXIT_FAILURE,stderr,argv[0]);
 				}
 
@@ -37,14 +42,19 @@ int main(int argc, char *argv[]){
 				break;
 			case 'e'://encrypt
 				if(dflag){
+					//exit out with error as it is mutually exclusive with decrypt option
 					usage(EXIT_FAILURE,stderr,argv[0]);
-					break;
 				}
+
 				de_or_en = 'e';
 				eflag++;
 				break;
 			case 'h'://help
 				usage(EXIT_SUCCESS,stdout,argv[0]);
+				break;
+			case 'f'://input file, must have filename argument
+				input_filename
+				printf("TEST: %s",optarg);
 				break;
 			default:
 				usage(EXIT_FAILURE,stderr,argv[0]);
